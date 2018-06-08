cd976dc00bee5d3f2ac145d0a59167562043b7cb
RoverYTR <40070804+RoverYTR@users.noreply.github.com>
2018-06-08 23:53:49 +0530

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -92,6 +92,24 @@ async def ping(ctx):
     					 description=str(ping.microseconds / 1000.0) + ' ms',
                          color=0x00ffff)
     await bot.say(embed=pong)
+    
+runSpam = true
+
+async def spamLoop(ctx):
+    """Pong! Check your response time."""
+    while runSpam :
+        await bot.say("spam")
+
+@bot.command(pass_context=True)
+async def startspam(ctx):
+    """Pong! Check your response time."""
+    runSpam = true
+    spamLoop(ctx)
+    
+@bot.command(pass_context=True)
+async def stopspam(ctx):
+    """Pong! Check your response time."""
+    runSpam = false
 
 @bot.command(pass_context=True)
 async def shutdown(ctx):
